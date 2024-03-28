6cd626e2804687110c3d340c716c2fca8b452b08
nesrin <nesrinnoufal99@gmail.com>
2024-03-28 17:57:07 +0530

    note app build version

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,10 @@ app.use(cors({origin: 'http://localhost:3000'}));
 
 app.use('/api/auth',AuthRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 // app.get('/', () =>{
