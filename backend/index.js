2d03897d93fbe643caa2cacdae465b2cfe3ca7e3
nesrin <nesrinnoufal99@gmail.com>
2024-03-28 17:55:12 +0530

    note app build version

--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,29 @@ import express from "express"
 import connectToMongodb from "./db/connectToMongodb.js";
 import AuthRoutes from './routes/auth.routes.js';
 import cors from 'cors';
-
+import path from "path";
 
 
 const app = express ();
 const PORT = 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors({origin: 'http://localhost:3000'}));
 
 app.use('/api/auth',AuthRoutes);
 
-app.get('/', () =>{
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+// app.get('/', () =>{
     
-    console.log("Server running ");
-})
+//     console.log("Server running ");
+// })
 
 app.listen(PORT, () => {
     connectToMongodb();
